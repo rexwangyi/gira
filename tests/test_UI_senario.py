5e93599d3f97e75ac3082227f5d99a2c726f5885
wangyi <wy@rayfaydata.com>
2024-12-29 16:30:30 +0800

    update

--- tests/test_UI_senario.py
+++ tests/test_UI_senario.py
@@ -26,6 +26,9 @@ chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
 chrome_options.add_argument(f"--user-data-dir={temp_dir}")
 chrome_options.add_argument("--verbose") 
 chrome_options.add_argument("--log-level=3")
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-shm-usage")
 
 
 # スクリーンショットの保存先を設定
